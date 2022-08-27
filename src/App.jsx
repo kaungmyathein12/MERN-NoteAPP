386b452e0d5f7db233530847c3b26e612c858243
kaungmyathein982 <kaungmyathein.dev@gmail.com>
2022-08-27 11:19:06 +0630

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import MyEditor from "./components/MyEditor";
 import UserProfile from "./pages/UserProfile";
@@ -6,60 +7,74 @@ import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import NoteDetails from "./components/NoteDetails";
 import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 function App() {
+  const [user, setUser] = useState(false);
   const [create, setCreate] = useState(false);
   const [detail, setDetail] = useState(false);
   return (
-    <div className="App h-screen overflow-hidden relative">
-      <div
-        className={
-          "bg-[#151515] h-full text-white font-poppins overflow-y-auto"
-        }
-      >
-        <AnimatePresence>
-          {create && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.4 }}
-              exit={{ opacity: 0 }}
-              className="absolute inset-0 h-full bg-black bg-opacity-80 grid place-items-center z-20"
-            >
+    <div className="App bg-[#151515] h-screen overflow-hidden relative">
+      <BrowserRouter>
+        <div className={"h-full text-white font-poppins overflow-y-auto"}>
+          <AnimatePresence>
+            {create && (
               <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                exit={{ scale: 0, opacity: 0 }}
-                className="w-11/12 md:w-2/6 bg-[#161616] h-auto relative rounded-md overflow-hidden"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.4 }}
+                exit={{ opacity: 0 }}
+                className="absolute inset-0 h-full bg-black bg-opacity-80 grid place-items-center z-20"
               >
-                <MyEditor setCreate={setCreate} />
+                <motion.div
+                  initial={{ scale: 0 }}
+                  animate={{ scale: 1 }}
+                  exit={{ scale: 0, opacity: 0 }}
+                  className="w-11/12 md:w-2/6 bg-[#161616] h-auto relative rounded-md overflow-hidden"
+                >
+                  <MyEditor setCreate={setCreate} />
+                </motion.div>
               </motion.div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-        <AnimatePresence>
-          {detail && (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.4 }}
-              exit={{ opacity: 0 }}
-              className="absolute inset-0 h-full bg-black bg-opacity-80 grid place-items-center z-20"
-            >
+            )}
+          </AnimatePresence>
+          <AnimatePresence>
+            {detail && (
               <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }}
-                exit={{ scale: 0, opacity: 0 }}
-                className="w-11/12 md:w-2/6 bg-[#161616] h-auto relative rounded-md overflow-hidden"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.4 }}
+                exit={{ opacity: 0 }}
+                className="absolute inset-0 h-full bg-black bg-opacity-80 grid place-items-center z-20"
               >
-                <NoteDetails setDetail={setDetail} />
+                <motion.div
+                  initial={{ scale: 0 }}
+                  animate={{ scale: 1 }}
+                  exit={{ scale: 0, opacity: 0 }}
+                  className="w-11/12 md:w-2/6 bg-[#161616] h-auto relative rounded-md overflow-hidden"
+                >
+                  <NoteDetails setDetail={setDetail} />
+                </motion.div>
               </motion.div>
-            </motion.div>
-          )}
-        </AnimatePresence>
-        <Header />
-        <UserProfile setCreate={setCreate} setDetail={setDetail} />
-        <Footer />
-      </div>
+            )}
+          </AnimatePresence>
+          {user && <Header />}
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <UserProfile /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/register"
+              element={!user ? <SignUp /> : <Navigate to="/" />}
+            />
+          </Routes>
+          {user && <Footer />}
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="h-full grid place-items-center">
+      <div className="w-11/12 md:w-2/5 xl:w-3/12 bg-[#191919] px-4 py-6 rounded-md">
+        <h3 className="bg-neutral-800 inline-block mb-5 text-white text-lg font-medium px-4 py-1 rounded-md">
+          NoteApp
+        </h3>
+        <br />
+        <input
+          type="text"
+          placeholder="Enter your email"
+          className="w-full bg-[#151515] p-3 font-base outline-none text-gray-400 rounded-md mb-4"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Enter your email"
+          className="w-full bg-[#151515] p-3 font-base outline-none text-gray-400 rounded-md mb-4"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="w-full bg-indigo-600 hover:bg-indigo-700 transition-all py-2 text-white font-medium rounded-md">
+          Login
+        </button>
+        <div className="text-center mt-4 text-neutral-400 text-sm underline">
+          <Link to="/register">Don't have an account?</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
